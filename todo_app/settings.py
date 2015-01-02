af13ae5b7f8d936a8c225e22a78ba0c592de032a
srvsngh200892 <srvsngh200892@gmail.com>
2015-01-03 01:57:16 +0530

    changed setting file

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+print BASE_DIR
 STATIC_DIR = os.path.join(BASE_DIR, "static")
 
 
@@ -110,15 +111,6 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # try to load local_settings.py if it exists
