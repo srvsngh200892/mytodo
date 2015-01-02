4f60c53934e47c4f3ad0458c5f083de03819c469
srvsngh200892 <srvsngh200892@gmail.com>
2015-01-03 01:08:19 +0530

    changed proc file

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -49,7 +49,8 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'todo',
     'storages',
-    'boto'
+    'boto',
+    'dj-database-url',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -109,7 +110,7 @@ STATIC_ROOT = 'staticfiles'
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 
-DATABASES = { 'default' : dj_database_url.parse('postgres://...')}
+DATABASES = {'default': dj_database_url.config(default='postgres://...')}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
